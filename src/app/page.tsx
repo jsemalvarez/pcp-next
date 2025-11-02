9887363d5627e780c1b16630f013c676803008b3
jsemalvarez <jsemalvarez@gmail.com>
2025-11-02 20:42:47 -0300

    fix: add HeroSection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,10 +5,9 @@ import { PublicNavbar } from "@/presentation/components/Navbar/PublicNavbar";
 export default function Home() {
   return (
     <>
-      {/* <PublicNavbar /> */}
+      <PublicNavbar />
       <main className="min-h-[100vh] bg-gradient-to-bl from-primary via-primary-light to-primary text-indigo-100">
-        {/* <HeroSection /> */}
-        <h2>hola mundo</h2>
+        <HeroSection />
       </main>
     </>
   );
