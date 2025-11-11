2621c4dee6c93fad3a93f0ca1081245f86b95af3
jsemalvarez <jsemalvarez@gmail.com>
2025-11-10 23:41:35 -0300

    feat: TitleSection.tsx

---
+++ src/presentation/components/common/TitleSection.tsx
@@ -0,0 +1,13 @@
+
+interface Props {
+  children: React.ReactNode
+  className?: string
+}
+
+export const TitleSection = ({children}:Props) => {
+  return (
+    <h2 className="text-center text-5xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-pink-400 to-orange-300 tracking-widest">
+        {children}
+    </h2>
+  )
+}
