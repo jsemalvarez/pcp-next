3d2d5b446b7a9144dbdd30e6961c6d80a02ff209
jsemalvarez <jsemalvarez@gmail.com>
2025-12-01 20:35:12 -0300

    style: add banner feriainfinita

--- src/data/slider/sliderImagesHero.ts
+++ src/data/slider/sliderImagesHero.ts
@@ -1,19 +1,19 @@
 export const sliderImagesHero = [
-    // {
-    //   imageName: "banner_techno_kids_avartt.webp",
-    //   title: "Techno Kids",
-    //   subTitle: "Primera fiesta electrónica para niños y familias",
-    // },
+    {
+      imageName: "feriainfinita/Feria_infinita_banner_p11hmz.webp",
+      title: "Feria Infinita",
+      subTitle: "Tienda de Ropa - Moda circular",
+    },
     {
       imageName: "banner_la_audacia_iagg08.webp", 
       title: "Circo La Audacia",
       subTitle: "Ahora en la Laguan de Los Padres",
     },
-    // {
-    //   imageName: "banner_hazme_reir_lrdzhz.webp",
-    //   title: "Circo HAZME REIR",
-    //   subTitle: "Plaza del agua",
-    // },
+    {
+      imageName: "banner_hazme_reir_lrdzhz.webp",
+      title: "Circo HAZME REIR",
+      subTitle: "Plaza del agua",
+    },
     {
       imageName: "banner_las_dos_marias_tvrbmm.webp",
       title: "Granja Las Dos Marias",
