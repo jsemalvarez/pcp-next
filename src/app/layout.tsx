267ba70d711faa97136d6816e7c0d91a65e3fcc4
jsemalvarez <jsemalvarez@gmail.com>
2025-12-02 18:17:44 -0300

    feat: add global openGraph

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,10 +14,25 @@ const baloo2 = Baloo_2({
 });
 
 export const metadata: Metadata = {
-title: 'Paseos con Peques | Guía de actividades en Mar del Plata',
+  title: 'Paseos con Peques | Guía de actividades en Mar del Plata',
   description: 'Descubrí los mejores lugares, eventos y paseos para disfrutar con niños, chicos o peques. Mapa interactivo y agenda cultural actualizada.',
   icons: {
     icon: "/favicon.svg",
+  },
+  openGraph: {
+    title: "Paseos con Peques",
+    description:"Descubrí los mejores lugares, eventos y paseos para disfrutar con niños, chicos o peques. Mapa interactivo y agenda cultural actualizada.",
+    url: "https://paseosconpeques.com.ar/",
+    type: "website",
+    locale: "es_ES",
+    images: [
+      {
+        url: "https://res.cloudinary.com/dwhdla1b4/image/upload/w_300,q_auto,f_auto/v1744900287/pcp-images/logo_pcp_mppj0w.webp",
+        width: 300,
+        height: 300,
+        alt: "Paseos con Peques",
+      },
+    ],
   },
 };
 
